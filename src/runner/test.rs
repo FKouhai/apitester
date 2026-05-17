12ed9bd32a11fa01b4e1d7759a192212fa08e0e6
FKouhai <frandres00@gmail.com>
2026-05-17 20:19:45 +0200

    chore(test_runner): deduplicate request result build up

--- src/runner/test.rs
+++ src/runner/test.rs
@@ -4,7 +4,7 @@ use futures::{StreamExt, stream::FuturesUnordered};
 
 use crate::{
     assert::{CheckResult, check_all},
-    config::ValidatedTestConfig,
+    config::{ValidatedAssert, ValidatedTestConfig},
     http::{self, HttpError, ResponseData},
 };
 
@@ -14,6 +14,28 @@ pub struct RequestResult {
     pub name: String,
     pub response: Result<ResponseData, HttpError>,
     pub assertions: Vec<CheckResult>,
+}
+
+fn into_request_result(
+    name: String,
+    assert: Option<&ValidatedAssert>,
+    res: Result<ResponseData, HttpError>,
+) -> RequestResult {
+    match res {
+        Ok(response) => {
+            let assertions = assert.map_or_else(Vec::new, |a| check_all(a, &response));
+            RequestResult {
+                name,
+                response: Ok(response),
+                assertions,
+            }
+        }
+        Err(e) => RequestResult {
+            name,
+            response: Err(e),
+            assertions: Vec::new(),
+        },
+    }
 }
 
 pub async fn run(
@@ -32,51 +54,17 @@ pub async fn run(
             });
         }
         while let Some((name, assert, res)) = futures.next().await {
-            match res {
-                Ok(response) => {
-                    let assertions = if let Some(a) = &assert {
-                        check_all(a, &response)
-                    } else {
-                        Vec::new()
-                    };
-                    results.push(RequestResult {
-                        name,
-                        response: Ok(response),
-                        assertions,
-                    });
-                }
-                Err(e) => results.push(RequestResult {
-                    name,
-                    response: Err(e),
-                    assertions: Vec::new(),
-                }),
-            }
+            results.push(into_request_result(name, assert.as_ref(), res));
         }
     } else {
         let client = http::build_client(config)?;
         for req in &config.requests {
             let res = http::execute(req, &client).await;
-            match res {
-                Ok(response) => {
-                    let assertions = if let Some(assert) = &req.assert {
-                        check_all(assert, &response)
-                    } else {
-                        Vec::new()
-                    };
-                    results.push(RequestResult {
-                        name: req.name.clone(),
-                        response: Ok(response),
-                        assertions,
-                    });
-                }
-                Err(e) => {
-                    results.push(RequestResult {
-                        name: req.name.clone(),
-                        response: Err(e),
-                        assertions: Vec::new(),
-                    });
-                }
-            }
+            results.push(into_request_result(
+                req.name.clone(),
+                req.assert.as_ref(),
+                res,
+            ));
         }
     }
     Ok(results)
